a18664052b4a6b2ee2fc47f3589cd307ff920b91
zawrayz <zoraizzia9@gmail.com>
2026-06-11 10:59:22 +0500

    Block and hide password change for portal employees

--- hr_employee_portal/controllers/password_security.py
+++ hr_employee_portal/controllers/password_security.py
@@ -2,38 +2,39 @@ from odoo import http
 from odoo.http import request
 from odoo.exceptions import AccessDenied
 from odoo.addons.portal.controllers.portal import CustomerPortal
+from odoo.addons.web.controllers.session import Session
 
 
-def _is_blocked_portal_employee():
+def _is_portal_employee_user():
     user = request.env.user
 
     if not user or user._is_public():
         return False
 
-    # Internal users/admins are allowed
+    # Admin/internal users are allowed
     if user.has_group("base.group_user"):
         return False
 
-    # Portal users are blocked
+    # Portal employees are blocked
     if user.has_group("base.group_portal"):
         return True
 
     return False
 
 
-class HrPortalPasswordSecurity(CustomerPortal):
+class HrPortalSecurityBlock(CustomerPortal):
 
     @http.route(['/my/security'], type='http', auth='user', website=True)
     def security(self, **kw):
-        if _is_blocked_portal_employee():
+        if _is_portal_employee_user():
             return request.redirect('/my/hr')
         return super().security(**kw)
 
 
-class HrPortalPasswordChangeBlock(http.Controller):
+class HrPortalSessionPasswordBlock(Session):
 
     @http.route('/web/session/change_password', type='json', auth='user')
     def change_password(self, fields):
-        if _is_blocked_portal_employee():
+        if _is_portal_employee_user():
             raise AccessDenied("Portal employees are not allowed to change their password.")
-        return request.env['ir.http']._dispatch()
+        return super().change_password(fields)
